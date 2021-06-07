ca80eca49ec93eb2d34985042f1dced9444164d6
rwrw96 <kazuki137@gmail.com>
2021-06-07 23:12:07 +0000

    test

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,18 @@
 class ReviewsController < ApplicationController
+    
+    def show
+      @review = Review.find(params[:id])
+    end
+    
+    def create
+      @review = Review.new(review_params)
+      @review.user_id = current_user.id
+      @review.save
+      redirect_to review_path(@review)
+    end
+
+private
+  def review_params
+    params.require(:review).permit(:title,:body,:movie_title,:release_date,:overview,:poster_path,:tmdb,:rate)
+  end
 end
